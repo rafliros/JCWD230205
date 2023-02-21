aa14baa50dc2f31d483f745230bfb2d2290dc8ae
Pamela <46244119+patriciamadelinek@users.noreply.github.com>
2023-02-21 17:57:17 +0700

    Add files via upload

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,27 +1,50 @@
-import axios from "axios";
-import logo from "./logo.svg";
-import "./App.css";
-import { useEffect, useState } from "react";
+// Import Router
+import { Routes, Route} from 'react-router-dom';
 
-function App() {
-  const [message, setMessage] = useState("");
+// Import Navbar
+import Navbar from "./components/navbar";
 
-  useEffect(() => {
-    (async () => {
-      const { data } = await axios.get(
-        `${process.env.REACT_APP_API_BASE_URL}/greetings`
-      );
-      setMessage(data?.message || "");
-    })();
-  }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {message}
-      </header>
-    </div>
-  );
+// Import Home
+import Home from "./pages/home/home";
+
+// Import Rooms
+import Rooms from "./pages/rooms/rooms";
+
+// Import Booking
+import Booking from "./pages/booking/booking";
+
+// Import Contact Us
+// import Contactus from "./pages/rooms/rooms";
+
+// Import Login
+import Login from "./pages/login/login";
+
+// Import Register
+import Signup from "./pages/signup/signup";
+
+// Import Footer
+import Footer from "./components/footer";
+
+// Import Register
+// import Register from "./pages/register/register";
+
+// import axios from 'axios';
+
+export default function App(){
+
+
+  return(
+    <>
+      <Navbar />
+        <Routes>
+          <Route path='/'  element={<Home />} />
+          <Route path='/rooms'  element={<Rooms />} />
+          <Route path='/booking'  element={<Booking />} /> 
+          <Route path='/login'  element={<Login />} />
+          <Route path='/signup'  element={<Signup />} />
+          {/* <Route path='/contactus'  element={<Contactus />} /> */}
+        </Routes>
+      <Footer />
+    </>
+  )
 }
-
-export default App;
